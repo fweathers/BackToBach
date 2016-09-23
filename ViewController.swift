114915902e92a4fff9369fc4b66842fb9f53e89d
Felicia Weathers <Fel.Weathers@gmail.com>
2016-09-23 13:59:24 -0400

    Adjust scrubberSlider to move with music, and fix stop button to restart the song once play button is tapped aftwerward, add mozart image

--- ViewController.swift
+++ ViewController.swift
@@ -13,19 +13,40 @@ class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
     let audioPath = Bundle.main.path(forResource: "mozart", ofType: "mp3")
+    var timer = Timer()
+    
+    func updateScrubber() {
+        
+        scrubberSlider.value = Float(player.currentTime)
+    }
 
     @IBAction func playButtonTapped(_ sender: UIBarButtonItem) {
         
         player.play()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
     }
     
     @IBAction func pauseButtonTapped(_ sender: UIBarButtonItem) {
         
         player.pause()
+        
+        timer.invalidate()
     }
     @IBAction func stopButtonTapped(_ sender: UIBarButtonItem) {
         
-        player.stop()
+        timer.invalidate()
+        
+        player.pause()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+                        
+        } catch {
+            
+            // process error
+        }
     }
     
     @IBAction func volumeSliderMoved(_ sender: UISlider) {
